c1c2449591ed7d045ac53f054b1c74a8338180c2
Faruk Yazir <farukyazir04@gmail.com>
2026-09-26 08:00:15 +0530

    fix netlify

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {  Outlet } from 'react-router-dom'
 import Header from './Header'
-import Navbar from './navbar'
+import Navbar from './Navbar'
 const Template = () => {
   return (
     <>
